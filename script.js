bbdee02baf020c23147cbc49d0553270ab052e09
Virgilio Sardido Reyes <virgilio.s.reyes@gmail.com>
2020-08-02 16:38:29 +0200

    Added progressbar

--- script.js
+++ script.js
@@ -25,16 +25,30 @@ function updatePlayIcon() {
 // Update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
 function stopVideo() {
-  video.curentTime = 0;
+  video.currentTime = 0;
   video.pause();
 }
 
